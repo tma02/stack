203867ea013619474634588543bd21eb287554eb
Tianyi Ma <tn.ma@outlook.com>
2016-03-06 21:20:02 -0600

    cool colors

--- game.js
+++ game.js
@@ -24,6 +24,13 @@ stack.gameCycle = function() {
       }
       else {
         stack.score++;
+        if (stack.blocks.length >= 9) {
+          var firstBlock = stack.blocks.shift();
+          var r = firstBlock._red;
+          var g = firstBlock._green;
+          var b = firstBlock._blue;
+          document.body.style.backgroundColor = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+        }
         stack.spawnBlock();
       }
       return;
@@ -61,9 +68,6 @@ stack.spawnBlock = function() {
   var y = 482;
   var right = true;
   var w = 400;
-  var r = 238;
-  var g = 238;
-  var b = 238;
   var lastBlock = stack.getLastBlock();
   if (typeof lastBlock !== 'undefined') {
     w = lastBlock.w;
@@ -73,13 +77,13 @@ stack.spawnBlock = function() {
     if (!right) {
       x = -Crafty.viewport.x + 400 - 3;
     }
-    r = Math.floor(lastBlock._red + (Math.random() * 10) - 5);
-    g = Math.floor(lastBlock._green + (Math.random() * 10) - 5);
-    b = Math.floor(lastBlock._blue + (Math.random() * 10) - 5);
+    stack.color.r = Math.floor(lastBlock._red + (Math.random() * 10) - 5);
+    stack.color.g = Math.floor(lastBlock._green + (Math.random() * 10) - 5);
+    stack.color.b = Math.floor(lastBlock._blue + (Math.random() * 10) - 5);
   }
   var block = Crafty.e('2D, Canvas, Color, Keyboard')
     .attr({i: stack.blocks.length, x: x, y: y, w: w, h: 30, right: right, move: true})
-    .color(r, g, b)
+    .color(stack.color.r, stack.color.g, stack.color.b)
     .bind('KeyDown', function(e) {
       if(e.key == Crafty.keys.SPACE) {
         this.move = false;
@@ -92,16 +96,22 @@ stack.spawnBlock = function() {
 };
 
 document.addEventListener('DOMContentLoaded', function(event) {
+  stack.color = {
+    r: 238,
+    g: 238,
+    b: 238
+  };
+  document.body.style.backgroundColor = 'rgb(' + stack.color.r + ', ' + stack.color.g + ', ' + stack.color.b + ')';
   Crafty.init(400, 540, document.getElementById('game'));
   Crafty.viewport.init(400, 540, document.getElementById('game'));
   Crafty.background('#fff');
   stack.blocks.push(Crafty.e('2D, Canvas, Color')
     .attr({i: stack.blocks.length, x: 0, y: 512, w: 400, h: 270, right: false, move: false})
-    .color('#eeeeee')
+    .color(stack.color.r, stack.color.g, stack.color.b)
   );
   Crafty.e('2D, Canvas, Color')
     .attr({x: -512, y: 512, w: 1424, h: 286})
-    .color('#eeeeee');
+    .color(stack.color.r, stack.color.g, stack.color.b);
   stack.tapElement = Crafty.e('2D, Canvas, Color, Mouse')
     .attr({x: -512, y: 0, w: 1424, h: 540})
     .color(0, 0, 0, 0)
